2d35065ba22cdd5df74e6e993c0617cbd6bed454
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-09-11 17:21:51 +0100

    Install Paramiko and GObject2
    Duplicity requires these packages to run successfully. Paramiko is an SSH
    library for Python, and GOBject2 installs Python bindings for GObject.

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,6 +1,12 @@
 class duplicity::packages {
   # Install the packages
   package {
-    ['duplicity', 'python-boto', 'gnupg']: ensure => present
+    ['duplicity',
+     'python-boto',
+     'gnupg',
+     'python-paramiko',
+     'python-gobject-2'
+    ]:
+      ensure => present
   }
 }

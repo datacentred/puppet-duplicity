17a74bd9e648ed7a75431ff64c4c894241bb00d7
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2014-09-19 14:06:10 +0100

    Install Paramiko and GObject2
    Without the `python-paramiko` or `python-gobject-2` packages installed,
    the installation of th Gio and SSH Duplicity backends will fail:
    
      Import of duplicity.backends.giobackend Failed: No module named gio
      Import of duplicity.backends.sshbackend Failed: No module named paramiko
    
    Install these two packages.
    
    For further details, see
    http://www.rfc3092.net/2013/09/missing-modules-for-paramiko-and-gio-in-duplicity-foo/

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,6 +1,12 @@
 class duplicity::packages {
   # Install the packages
-  package {
-    ['duplicity', 'python-boto', 'gnupg']: ensure => present
+  package {[
+            'duplicity',
+            'python-paramiko',
+            'python-gobject-2',
+            'python-boto',
+            'gnupg'
+            ]:
+          ensure => present
   }
 }

47c5b2a7d25297465b28ad7e0eb941c3a2441b7e
Simon Murray <simon.murray@datacentred.co.uk>
2015-06-19 13:24:37 +0100

    Redirect stdout spam into logging

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,4 +1,13 @@
 class duplicity::packages {
   # Install the packages
   ensure_packages (['duplicity', 'python-boto', 'gnupg'])
+
+  # Install the log directory.  stdout ends up here stderr is
+  # free to get mailed out by cron
+  file { '/var/log/duplicity':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0770',
+  }
 }
